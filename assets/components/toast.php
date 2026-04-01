9854a647af3990b38e69b849be422c012bdd744c
jhnncarl <nudalojhoncarloe@gmail.com>
2026-04-01 14:26:16 +0800

    Updated Toast Notification

--- assets/components/toast.php
+++ assets/components/toast.php
@@ -1,21 +1,2 @@
-<!-- Toast Notification Component -->
-<div id="toast-container" class="toast-container">
-    <div id="otp-toast" class="toast toast-success">
-        <div class="toast-icon">
-            <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
-                <path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path>
-                <polyline points="22 4 12 14.01 9 11.01"></polyline>
-            </svg>
-        </div>
-        <div class="toast-content">
-            <div class="toast-title">Success</div>
-            <div class="toast-message">OTP code has been sent to your email address</div>
-        </div>
-        <button class="toast-close" id="toastClose">
-            <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
-                <line x1="18" y1="6" x2="6" y2="18"></line>
-                <line x1="6" y1="6" x2="18" y2="18"></line>
-            </svg>
-        </button>
-    </div>
-</div>
+<!-- Toast Notification Container -->
+<div id="toast-container" class="toast-container"></div>
