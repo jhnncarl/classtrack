962065fb5486d5504f3bf3ec57c0f28e5d945915
jhnncarl <nudalojhoncarloe@gmail.com>
2026-05-14 18:20:39 +0800

    Removing custom js

--- student/attendance.php
+++ student/attendance.php
@@ -618,8 +618,6 @@ $user_initials = strtoupper(substr($user_name, 0, 2));
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     <!-- Include Toast Component -->
     <?php include '../assets/components/toast.php'; ?>
-    <!-- Custom JavaScript -->
-    <script src="../assets/js/sidebar.js"></script>
     <script src="../assets/js/attendance.js?v=<?php echo time(); ?>"></script>
     
     <?php if (!$canViewAttendanceRecord): ?>
